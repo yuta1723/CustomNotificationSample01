a21ec392953e7ad2e170a8154ee1682f1dbe022e
yuta naito <y.naito@suvt.co.jp>
2017-06-29 13:38:00 +0900

    notification のbuttonにonClickListenrを設定したが、アプリ起動時にNPEが発生するようになってしまった

--- app/src/main/java/com/ynaito/customnotificationsample/MainActivity.java
+++ app/src/main/java/com/ynaito/customnotificationsample/MainActivity.java
@@ -1,12 +1,13 @@
 package com.ynaito.customnotificationsample;
 
 import android.app.Notification;
-import android.content.Intent;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.NotificationCompat;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RemoteViews;
 
 import java.util.Random;
@@ -14,6 +15,9 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getSimpleName();
+    private Button seekRewindButton;
+    private Button playButton;
+    private Button seekFastForwardButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
         NotificationManagerCompat manager = NotificationManagerCompat.from(this.getApplicationContext());
 
         manager.notify(generateNotificationId(), notification);
+
+        seekFastForwardButton = (Button) findViewById(R.id.button_foward);
+        seekFastForwardButton.setOnClickListener(seekRewindButtonClickListener);
+        playButton = (Button) findViewById(R.id.button_play);
+        playButton.setOnClickListener(playButtonClickListener);
+        seekRewindButton = (Button) findViewById(R.id.button_rewind);
+        seekRewindButton.setOnClickListener(seekFastForwardButtonClickListener);
     }
 
     private Notification generateNotification(RemoteViews remoteViews) {
@@ -32,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
         builder.setSmallIcon(R.mipmap.ic_launcher);
         builder.setVisibility(Notification.VISIBILITY_PUBLIC);
         builder.setContent(remoteViews);
+//        builder.setCustomBigContentView(remoteViews);
+        builder.setStyle(new NotificationCompat.MediaStyle().setMediaSession(null));
         return builder.build();
     }
 
@@ -41,4 +54,25 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "notificationId : " + ran);
         return ran;
     }
+
+    private View.OnClickListener seekRewindButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.d(TAG, "seekRewindButton is Clicked");
+        }
+    };
+
+    private View.OnClickListener playButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.d(TAG, "seekPlayButton is Clicked");
+        }
+    };
+
+    private View.OnClickListener seekFastForwardButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.d(TAG, "seekFastForwardButton is Clicked");
+        }
+    };
 }
