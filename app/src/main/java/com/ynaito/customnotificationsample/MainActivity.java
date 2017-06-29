2cfa5869bcb79172289c5f61e351e72aaa39359f
yuta naito <y.naito@suvt.co.jp>
2017-06-29 10:58:08 +0900

    create notification layout and remoteViews instance

--- app/src/main/java/com/ynaito/customnotificationsample/MainActivity.java
+++ app/src/main/java/com/ynaito/customnotificationsample/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.custom_notification);
     }
 }
